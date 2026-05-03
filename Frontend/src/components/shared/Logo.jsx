6f96d9df6361c04f32167dd9ee205153c3ffed4e
Vaibhav Zine <vzine91@gmail.com>
2026-05-03 15:01:29 +0530

    Fix logo asset import for Vercel

--- Frontend/src/components/shared/Logo.jsx
+++ Frontend/src/components/shared/Logo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import expenseLogo from "@/assets/expenselogo.png";
 
-const Logo = () =>{
-    return(
-        <Link to="/">
-            <img src="./src/assets/expenselogo.png" alt="logo" className="w-15"></img>
-        </Link>
-    )
-}
+const Logo = () => {
+  return (
+    <Link to="/">
+      <img src={expenseLogo} alt="logo" className="w-15"></img>
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;
