50d89264df38cbf9e0aea236620e465a808db2bd
Vaibhav Zine <vzine91@gmail.com>
2026-05-03 14:21:50 +0530

    fix logo import

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import Logo from "./shared/logo";
+import Logo from "./shared/Logo";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
